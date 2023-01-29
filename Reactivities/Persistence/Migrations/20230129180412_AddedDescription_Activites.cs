07a8e00ce4ca54a064130141f869793d247f7fb6
HS\himanshu.rai <himanshu.rai@heliossolutions.co>
2023-01-30 00:01:44 +0530

    End of Section 2

---
+++ Reactivities/Persistence/Migrations/20230129180412_AddedDescription_Activites.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedDescriptionActivites : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Activities",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Activities");
+        }
+    }
+}
